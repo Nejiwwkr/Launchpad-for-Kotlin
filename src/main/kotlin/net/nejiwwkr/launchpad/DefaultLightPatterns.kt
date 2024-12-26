a1feec4164ed6ae10de0d502d60828ac5360545f
Nejiwwkr <xlijingtao123456x@163.com>
2024-12-26 22:02:53 +0800

    add more extension function

--- src/main/kotlin/net/nejiwwkr/launchpad/DefaultLightPatterns.kt
+++ src/main/kotlin/net/nejiwwkr/launchpad/DefaultLightPatterns.kt
@@ -3,37 +3,106 @@ package net.nejiwwkr.launchpad
 /**
  * set a listener for cross pattern light.
  */
-fun ConcurrentLaunchpad.setOnCrossPatternListener(pos: Int, color: Int){
+fun ConcurrentLaunchpad.setOnCrossPatternListener(pos: Int, color: Int, frequency: Long, internal: Long){
     this.setOnListener(pos) { _, velocity ->
-        if (velocity > 0) {
-            val type = LightType.STATIC
-            this.sendFeedbackMessage(0x90, pos, color)
-            Thread.sleep(50L)
-            for (i in 1..8) {
-                if (pos + 10 * i < 100)
-                    this.sendFeedbackMessage(type, pos + 10 * i, color)
-                if (pos - 10 * i >= 11)
-                    this.sendFeedbackMessage(type, pos - 10 * i, color)
-                if ((pos + i) - (pos + i) % 10 == pos - pos % 10)
-                    this.sendFeedbackMessage(type, pos + i, color)
-                if ((pos - i) - (pos - i) % 10 == pos - pos % 10)
-                    this.sendFeedbackMessage(type, pos - i, color)
-                Thread.sleep(50L)
-            }
+        if (velocity <= 0) return@setOnListener
 
-            this.sendFeedbackMessage(0x90, pos, 0)
-            Thread.sleep(50L)
-            for (i in 1..8) {
-                if (pos + 10 * i < 100 && color == this.getCurrentLight(pos + 10 * i))
-                    this.sendFeedbackMessage(type, pos + 10 * i)
-                if (pos - 10 * i >= 11 && color == this.getCurrentLight(pos - 10 * i))
-                    this.sendFeedbackMessage(type, pos - 10 * i)
-                if ((pos + i) - (pos + i) % 10 == pos - pos % 10 && color == this.getCurrentLight(pos + i))
-                    this.sendFeedbackMessage(type, pos + i)
-                if ((pos - i) - (pos - i) % 10 == pos - pos % 10 && color == this.getCurrentLight(pos - i))
-                    this.sendFeedbackMessage(type, pos - i)
-                Thread.sleep(50L)
-            }
+        val type = LightType.STATIC
+
+        this.sendFeedbackMessage(type, pos, color)
+        Thread.sleep(frequency)
+        for (i in 1..8) {
+            if (pos + 10 * i < 100)
+                this.sendFeedbackMessage(type, pos + 10 * i, color)
+            if (pos - 10 * i > 10)
+                this.sendFeedbackMessage(type, pos - 10 * i, color)
+            if ((pos + i) / 10 == pos / 10)
+                this.sendFeedbackMessage(type, pos + i, color)
+            if ((pos - i) / 10 == pos / 10)
+                this.sendFeedbackMessage(type, pos - i, color)
+            Thread.sleep(frequency)
+        }
+
+        Thread.sleep(internal)
+
+        this.sendFeedbackMessage(0x90, pos, 0)
+        Thread.sleep(frequency)
+        for (i in 1..8) {
+            if (pos + 10 * i < 100 && color == this.getCurrentLight(pos + 10 * i))
+                this.sendFeedbackMessage(type, pos + 10 * i)
+            if (pos - 10 * i > 10 && color == this.getCurrentLight(pos - 10 * i))
+                this.sendFeedbackMessage(type, pos - 10 * i)
+            if ((pos + i) / 10 == pos / 10 && color == this.getCurrentLight(pos + i))
+                this.sendFeedbackMessage(type, pos + i)
+            if ((pos - i) / 10 == pos / 10 && color == this.getCurrentLight(pos - i))
+                this.sendFeedbackMessage(type, pos - i)
+            Thread.sleep(frequency)
+        }
+    }
+}
+
+/**
+ * set a listener for horizontal line pattern light.
+ */
+fun ConcurrentLaunchpad.setOnHorizontalLinePatternListener(pos: Int, color: Int, frequency: Long, internal: Long){
+    this.setOnListener(pos) { _, velocity ->
+        if (velocity <= 0) return@setOnListener
+
+        val type = LightType.STATIC
+
+        this.sendFeedbackMessage(type, pos, color)
+        Thread.sleep(frequency)
+        for (i in 1..8) {
+            if (pos + 10 * i < 100)
+                this.sendFeedbackMessage(type, pos + 10 * i, color)
+            if (pos - 10 * i > 10)
+                this.sendFeedbackMessage(type, pos - 10 * i, color)
+            Thread.sleep(frequency)
+        }
+
+        Thread.sleep(internal)
+
+        this.sendFeedbackMessage(0x90, pos, 0)
+        Thread.sleep(frequency)
+        for (i in 1..8) {
+            if (pos + 10 * i < 100 && color == this.getCurrentLight(pos + 10 * i))
+                this.sendFeedbackMessage(type, pos + 10 * i)
+            if (pos - 10 * i > 10 && color == this.getCurrentLight(pos - 10 * i))
+                this.sendFeedbackMessage(type, pos - 10 * i)
+            Thread.sleep(frequency)
+        }
+    }
+}
+
+/**
+ * set a listener for vertical line pattern light.
+ */
+fun ConcurrentLaunchpad.setOnVerticalLinePatternListener(pos: Int, color: Int, frequency: Long, internal: Long){
+    this.setOnListener(pos) { _, velocity ->
+        if (velocity <= 0) return@setOnListener
+
+        val type = LightType.STATIC
+
+        this.sendFeedbackMessage(type, pos, color)
+        Thread.sleep(frequency)
+        for (i in 1..8) {
+            if ((pos + i) / 10 == pos / 10)
+                this.sendFeedbackMessage(type, pos + i, color)
+            if ((pos - i) / 10 == pos / 10)
+                this.sendFeedbackMessage(type, pos - i, color)
+            Thread.sleep(frequency)
+        }
+
+        Thread.sleep(internal)
+
+        this.sendFeedbackMessage(0x90, pos, 0)
+        Thread.sleep(frequency)
+        for (i in 1..8) {
+            if ((pos + i) / 10 == pos / 10 && color == this.getCurrentLight(pos + i))
+                this.sendFeedbackMessage(type, pos + i)
+            if ((pos - i) / 10 == pos / 10 && color == this.getCurrentLight(pos - i))
+                this.sendFeedbackMessage(type, pos - i)
+            Thread.sleep(frequency)
         }
     }
 }
